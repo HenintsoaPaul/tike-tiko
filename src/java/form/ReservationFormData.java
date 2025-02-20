d106deee0bd2d3c078572f472d79a50a0d31c2e2
Henintsoa Paul <henintsoapaul@gmail.com>
2025-02-20 03:51:04 +0300

    [reservation][fun]-Add dto ReservationFormData

---
+++ src/java/form/ReservationFormData.java
@@ -0,0 +1,53 @@
+package form;
+
+import src.summer.annotations.form.validation.Required;
+
+import java.time.LocalDateTime;
+
+public class ReservationFormData {
+
+    @Required
+    int id_vol;
+
+    @Required
+    int id_type_siege;
+
+    @Required
+    String nom_client;
+
+    @Required
+    LocalDateTime date_reservation;
+
+    // getters n setters
+    public LocalDateTime getDate_reservation() {
+        return date_reservation;
+    }
+
+    public void setDate_reservation(LocalDateTime date_reservation) {
+        this.date_reservation = date_reservation;
+    }
+
+    public int getId_type_siege() {
+        return id_type_siege;
+    }
+
+    public void setId_type_siege(int id_type_siege) {
+        this.id_type_siege = id_type_siege;
+    }
+
+    public String getNom_client() {
+        return nom_client;
+    }
+
+    public void setNom_client(String nom_client) {
+        this.nom_client = nom_client;
+    }
+
+    public int getId_vol() {
+        return id_vol;
+    }
+
+    public void setId_vol(int id_vol) {
+        this.id_vol = id_vol;
+    }
+}
